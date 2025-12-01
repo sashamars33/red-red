a48aca27c51e138dd31504fe83861304c4af3874
Sasha Marshall <sashamarshall.dev@gmail.com>
2025-12-01 15:16:28 -0800

    added collectable models and textures

--- Assets/Scripts/SceneChangerDoor.cs
+++ Assets/Scripts/SceneChangerDoor.cs
@@ -1,20 +1,44 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChangerOnCollision : MonoBehaviour, IInteractable
 {
     [Header("Add Loading Scene")]
     public string sceneToLoad;
+    public Image blackScreenImage; 
 
     public void Interact()
     {
         if (!string.IsNullOrEmpty(sceneToLoad))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(SceneLoadTimer(1));
+            
         }
         else
         {
             Debug.LogError("DoorInteractable: No scene assigned!");
         }
+    }
+
+    IEnumerator SceneLoadTimer(int scene)
+    {
+        float timer = 0f;
+        float duration = 0.5f;
+
+        while(timer < duration)
+        {
+            timer += Time.unscaledDeltaTime;
+            float lerp = timer / duration;
+
+            blackScreenImage.color = Color.Lerp(Color.clear, Color.black, lerp);
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(0.8f);
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
